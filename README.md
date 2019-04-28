713f275c8f8a02eb622abfd3c144c00f55b6f013
Mats Alm <mats.alm@academicwork.se>
2019-04-28 23:00:16 +0200

    modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # eventstore-stream-subscriber
+
+```javascript
+var consumer = require("eventstore-stream-consumer");
+// register handlers
+consumer.registerHanler("myEventType", function(subscription, event){
+    //handle event here...
+})
+
+var settings = {};
+consumer.createConnection({}, "admin", "changeIt").then(function(){
+    consumer.subscribeToStream("myStream");
+});
+
+```
--- app/eventstore-eventhandler.js
+++ app/eventstore-eventhandler.js
@@ -3,15 +3,15 @@ var EventsEmitter = require("events");
 class MyEmitter extends EventsEmitter{}
 const eventHandlers = new MyEmitter();
 
-function registerHandler(eventType, handler){
+function registerHandler(eventType, shandler){
     if(!(typeof(handler) === 'function')){
         throw new Error("argument 'handler' must be a function");
     }
     eventHandlers.on(eventType, handler);
 }
 
-function emitEvent(eventType, data){
-    eventHandlers.emit(eventType, data);
+function emitEvent(eventType, subscription, data){
+    eventHandlers.emit(eventType, subscription, data);
 }
 
 module.exports.registerHandler = registerHandler;
